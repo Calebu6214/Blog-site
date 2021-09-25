125e6e5afb2a16d59b7cdeea8fc2c5e9aca9021e
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-25 11:38:40 +0300

    add route for landing page after login

---
+++ app/main/views.py
@@ -0,0 +1,26 @@
+from flask import render_template, request, redirect, url_for
+from flask.helpers import flash
+from . import main
+from .. import db
+from ..models import User, Comment, Blog, Subscriber
+from flask_login import login_required, current_user
+from datetime import datetime
+from ..requests import get_quote
+from ..email import mail_message
+from .forms import BlogForm, CommentForm, UpdateBlogForm, UserProfile
+
+# 1 the main index(default)
+@main.route("/", methods=["GET", "POST"])
+def index():
+    blogs = Blog.get_all_blogs()
+    quote = get_quote()
+
+    if request.method == "POST":
+        new_sub = Subscriber(email=request.form.get("subscriber"))
+
+        db.session.add(new_sub)
+        db.session.commit()
+        mail_message("Thank you for subscribing to Glam-bay blogs",
+                     "email/welcome", new_sub.email)
+
+    return render_template("index.html", blogs=blogs, quote=quote)
