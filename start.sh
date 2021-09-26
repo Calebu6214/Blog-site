307413159632ab7970e87d78a3262d41e6bbc03c
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-26 12:28:08 +0300

    new changes

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MAIL_USERNAME='calebkimutai97@gmail.com'
 export MAIL_PASSWORD='Caleb254#'
-export SECRET_KEY='caleb'
+export SECRET_KEY='caleb12'
 export DEBUG=True
 
 python3.8 manage.py server
